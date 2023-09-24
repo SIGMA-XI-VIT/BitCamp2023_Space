0a3b5b89b224e3a6f49d192d591329f04bd6dfd1
Aarjav Jain <aarjavkjain2@gmail.com>
2023-09-24 15:14:14 +0530

    Replacing matplotlib with plotly

--- code.py
+++ code.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import plotly.graph_objects as go
 
 class SpaceDebrisSimulator:
     def __init__(self, master):
@@ -19,9 +18,10 @@ class SpaceDebrisSimulator:
         self.label_velocity.grid(row=1, column=0, sticky="w")
         self.entry_velocity.grid(row=1, column=1)
         self.button_simulate.grid(row=2, column=0, columnspan=2)
-        self.figure, self.ax = plt.subplots()
-        self.canvas = FigureCanvasTkAgg(self.figure, master=master)
-        self.canvas.get_tk_widget().grid(row=3, column=0, columnspan=2)
+        self.figure = go.FigureWidget()
+        self.figure.update_layout(title="Debris Motion", xaxis_title="Time", yaxis_title="Position")
+        self.figure_widget = self.figure.to_tk()
+        self.figure_widget.grid(row=3, column=0, columnspan=2)
         
     def simulate_motion(self):
         try:
@@ -29,25 +29,20 @@ class SpaceDebrisSimulator:
             initial_velocity = tuple(map(float, self.entry_velocity.get().split(',')))
             debris = SpaceDebris(1, initial_position, initial_velocity)
             delta_t = 1  # Time step (in seconds)
-            for _ in range(10):
-                debris.update_position(delta_t)
-                messagebox.showinfo("Debris Position", f"Debris {debris.id} Position: {debris.position}")
             positions = []  # List to store positions for plotting
             for _ in range(10):
                 debris.update_position(delta_t)
                 positions.append(debris.position)
+                messagebox.showinfo("Debris Position", f"Debris {debris.id} Position: {debris.position}")
             self.plot_positions(positions)
         except Exception as e:
             messagebox.showerror("Error", f"An error occurred: {e}")
-            
+        
     def plot_positions(self, positions):
-        self.ax.clear()
         x, y, z = zip(*positions)
-        self.ax.plot(x, label='x')
-        self.ax.plot(y, label='y')
-        self.ax.plot(z, label='z')
-        self.ax.legend()
-        self.canvas.draw()
+        self.figure.add_trace(go.Scatter(x=list(range(len(x))), y=x, mode='lines', name='x'))
+        self.figure.add_trace(go.Scatter(x=list(range(len(y))), y=y, mode='lines', name='y'))
+        self.figure.add_trace
 
 class SpaceDebris:
     def __init__(self, id, position, velocity):
